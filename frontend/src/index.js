8748598f1149326a22133f41b0108fbccfcf7944
victorpaularony <smallypauls@gmail.com>
2025-04-15 16:13:25 +0300

    index.js: for rendaring the application

---
+++ frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from './context/ThemeContext';
+
+import App from './App';
+import './styles/global.css';
+
+// Render the application
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
